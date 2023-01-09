0b975e686ba1d93fa717056f0ee9863ae5dc02bd
ElCurdente <jules.mesnil@gmail.com>
2023-01-09 11:31:43 +0100

    variables

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import ('./assets/global.css')
+import ('./assets/global.scss')
+import Vue3MobileDetection from "vue3-mobile-detection";
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.use(Vue3MobileDetection);
+app.use(router).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <h1>Hello les Oranges</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident, totam!</p>
+    <div v-if="$isMobile()">MOBILE</div>
+    <div v-else>DESKTOP OR TABLET</div>
   </div>
 </template>
 
