cd3d1d93022ce8aa0558bba5b7505be539d06b50
ElCurdente <jules.mesnil@gmail.com>
2023-01-09 11:34:13 +0100

    variables

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>Hello les Oranges</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident, totam!</p>
+    <p class="secondary">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident, totam!</p>
     <div v-if="$isMobile()">MOBILE</div>
     <div v-else>DESKTOP OR TABLET</div>
   </div>
