5f29f16a59bbae7a60539ab5f14a3950d8004d4a
ElCurdente <jules.mesnil@gmail.com>
2023-01-11 14:27:20 +0100

    Auto stash before merge of "feat-anecdote" and "origin/feat-anecdote"

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,4 @@
-<template>
+  <template>
     <div class="headerContainer">
         <div class="titleContainer">
             <h1 class="title">{{ title }}</h1>
--- src/views/AnecdoteView.vue
+++ src/views/AnecdoteView.vue
@@ -1,17 +1,22 @@
 <template>
     <div v-if="painting">
         <!-- header here -->
-        <section>
+        <section class="letsgo">
             <Header title="Multi Talented"></Header>
             <div class="anecdote-text">
-                <p>Derain can be seen as a multi-talented artist! He painted, drew and sculpted. He also made sets and costumes for theaters and ballets!</p>
-                <p>He amazes by his practical spirit, he liked to repair cars, to practice music, to play the piano or the organ, to build airplane maquettes or to draw tarot cards... He was an amazing artist who liked to discover
+                <p>Derain can be seen as a multi-talented artist! He painted, drew and sculpted. He also made
+                    sets and
+                    costumes for theaters and ballets!</p>
+                <p>He amazes by his practical spirit, he liked to repair cars, to practice music, to play the piano or
+                    the organ, to build airplane maquettes or to draw tarot cards... He was an amazing artist who liked
+                    to discover
                     <span>new things !<img src="/media/doodles/scratch.svg" alt=""></span>
                 </p>
             </div>
             <div class="artist-content">
-                <img class="artist-img" :src="'/media/anecdote/' + id  +'/artist.png'" alt="Derain">
-                <img class="signature-img" :src="'/media/anecdote/' + id  +'/signature.png'" alt="Signature from Derain">
+                <img class="artist-img" :src="'/media/anecdote/' + id + '/artist.gif'" alt="Derain">
+                <img class="signature-img" :src="'/media/anecdote/' + id + '/signature.png'"
+                    alt="Signature from Derain">
             </div>
 
             <router-link v-if="nextPage < lastPage" :to="'/map/' + nextPage" class="bubble__next">
@@ -21,6 +26,9 @@
                 Last Page
             </router-link>
                 
+        </section>
+        <section class="gsap-animation-container" id="sticky">
+            <div class="gsap-animation"></div>
         </section>
     </div>
     <div v-else>
@@ -32,9 +40,12 @@
 import Header from "../components/Header.vue"
 import paintings from "../../data/db.json"
 
+// let frame_count = 9,
+//     offset_value = 100;
+
 export default {
     props: ['id'],
-    components: {Header},
+    components: { Header },
     data() {
         return {
             nextPage: 0,
@@ -58,65 +69,86 @@ export default {
 </script>
 
 <style lang="scss">
+section {
+    height: 100vh;
+    width: 100vw;
+}
 
-    section {
-        height: 100vh;
-    }
+.anecdote-text {
+    margin: 0 32px;
+    transform: translateY(calc(20% - 30px));
 
-    .anecdote-text{
-        margin: 0 32px;
-        transform: translateY(-10px);
+    p {
+        line-height: 1.5em;
+        text-align: justify;
 
-        p{
-            line-height: 1.5em;
-            text-align: justify;
-            
-            &:nth-of-type(1){
-                margin-bottom: 1.5rem;
-            }
+        &:nth-of-type(1) {
+            margin-bottom: 1.5rem;
+        }
 
-            span {
-                position: relative;
+        span {
+            position: relative;
+            white-space: nowrap;
 
-                img {
-                    position: absolute;
-                    top: 1rem;
-                    left: 0;
-                    width: 100%
-                }
+            img {
+                position: absolute;
+                top: 1.1rem;
+                left: 0;
+                width: 100%
             }
         }
     }
-    
-    .artist-content {
-        position: relative;
-        height: calc(100vh - 257px);
+}
 
-        .artist-img {
-            position: absolute;
-            z-index: 10;
-            margin-left: auto;
-            height: 100%;
-            /* transform: translateY(10px) scale(1.1); */
-        }
-        .signature-img {
-            position: absolute;
-            bottom: 10rem;
-            left: 0;
-            animation: float 0.75s ease-in-out infinite;
-        }
+.artist-content {
+    position: absolute;
+    height: 50%;
+    bottom: 0;
+    width: 100%;
 
-        @keyframes float {
-            0% {
-                transform: translate(0, 0);
-            }
-            50% {
-                transform: translate(0, 10px);
-            }
-            100% {
-                transform: translate(0, 0);
-            }
-        }
+    .artist-img {
+        position: absolute;
+        right: 30px;
+        z-index: 10;
+        height: 100%;
+        transform: translateY(150px) scale(1.5);
     }
 
+    .signature-img {
+        position: absolute;
+        bottom: -10%;
+        left: 0;
+        animation: float 0.75s ease-in-out infinite;
+    }
+
+    @keyframes float {
+        0% {
+            transform: translate(0, 0);
+        }
+
+        50% {
+            transform: translate(0, 10px);
+        }
+
+        100% {
+            transform: translate(0, 0);
+        }
+    }
+}
+
+.gsap-animation-container {
+    height: 100vh;
+    width: 100%;
+}
+
+.gsap-animation {
+    height: 100%;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 200px;
+    width: 100%;
+    background-image: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/doodle-sprite.png);
+    background-repeat: no-repeat;
+    background-position: 0 50%;
+}
 </style>
