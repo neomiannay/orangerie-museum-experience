1c1f7929fe52de5344cd029c70fc0198fa7ef49b
ElCurdente <jules.mesnil@gmail.com>
2023-01-13 11:09:21 +0100

    fix: gif intro

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,7 @@
 <template>
   <div v-motion-fade :delay="200" class="home">
-    <img class="big-star" src="/media/doodles/big-stars.svg" alt="">
     <div class="logoWrapper">
-      <img class="logo" src="/media/intro/logo.png" alt="">
-      <img class="quote" src="/media/intro/quote.svg" alt="">
-      <img class="trait" src="/media/intro/trait.svg" alt="">
-      <img class="star star-1" src="/media/doodles/back-star-1.svg" alt="">
-      <img class="star star-2" src="/media/doodles/back-star-2.svg" alt="">
     </div>
-    <img class="loupe" src="/media/intro/loupe.png" alt="">
     <div class="logoEMWrapper">
       <img class="logo-EM" src="/media/intro/logoEM.png" alt="">
     </div>
@@ -40,63 +33,13 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 60%;
+    height: 100%;
+    width: 100%;
+    background-image: url('/media/intro/intro.gif');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
 
-    .logo {
-      width: 100%;
-    }
-
-    .quote {
-      position: absolute;
-      top: 0;
-      right: -10%;
-      animation: move 0.5s infinite;
-      animation-timing-function: steps(2, end);
-    }
-
-    .trait {
-      position: absolute;
-      bottom: -10%;
-      left: 1%;
-      animation: move 0.4s infinite;
-      animation-timing-function: steps(2, end);
-    }
-
-    .star {
-      position: absolute;
-      z-index: -1;
-      animation-timing-function: steps(2, end);
-    }
-
-    .star-1 {
-      bottom: -60px;
-      right: 20px;
-      animation: star 0.5s infinite;
-    }
-
-    .star-2 {
-      bottom: 0;
-      right: 0;
-      animation: star 0.3s infinite;
-    }
-  }
-
-  .big-star {
-    position: absolute;
-    top: 0;
-    right: 0;
-    z-index: -1;
-  }
-
-  .loupe {
-    position: absolute;
-    top: 50%;
-    left: 0;
-    transform: translateY(-80%);
-    transform-origin: left;
-    width: 45%;
-    animation: loupe 0.7s infinite;
-    animation-timing-function: steps(2, end);
   }
 
   .logoEMWrapper {
